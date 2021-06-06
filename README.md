9548769374c04f0977f908029affca895ae1ef84
Kevin Coleman <kevin.coleman@sparkstart.io>
2021-06-06 12:33:15 -0400

    first

---
+++ README.md
@@ -0,0 +1,26 @@
+## NSFW Flask detection
+
+Flask app for detecting NSFW images. Built using the models in [GantMan/nsfw_model](https://github.com/GantMan/nsfw_model)
+
+### Requirements
+- python 3.8.10
+
+## Quick start
+### prediction
+```
+curl -XPOST 'http://localhost:5000/predict?url=https://www.kcoleman.me/images/magnify-search.jpg'
+{
+  "drawings": 0.006791549269109964,
+  "hentai": 0.002260813256725669,
+  "neutral": 0.9589748978614807,
+  "porn": 0.016522442921996117,
+  "sexy": 0.015450258739292622
+}
+```
+### health check
+```
+curl http://localhost:5000/health
+{ "status": "ok }
+```
+
+## hosting - Heroku
---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask
+from flask import request
+from nsfw_detector import predict
+import requests
+import random
+import string
+import shutil 
+import os
+
+app = Flask(__name__)
+
+MODEL_PATH = 'mobilenet_v2_140_224'
+letters = string.ascii_lowercase
+
+model = predict.load_model(MODEL_PATH)    
+
+def download_image(url, dest):
+    r = requests.get(url, stream = True)
+    if r.status_code == 200:
+        # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
+        r.raw.decode_content = True
+        
+        # Open a local file with wb ( write binary ) permission.
+        with open(dest,'wb') as f:
+            shutil.copyfileobj(r.raw, f)
+        return True
+    return False
+
+@app.route('/health', methods=['GET'])
+def health():
+  return {'status': 'ok'}
+
+@app.route('/predict', methods=['POST'])
+def upload():
+    file_path = ''.join(random.choice(letters) for i in range(10))
+    download_image(request.args.get('url'),file_path)
+    res = predict.classify(model, file_path, predict.IMAGE_DIM)
+    os.remove(file_path) 
+
+    # Call the prediction function
+    return res[file_path]
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
